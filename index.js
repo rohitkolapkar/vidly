3ace406f61614ab39c90f232b7d331d591c159de
rohitkolapkar <rohitkolapkar333@gmail.com>
2019-03-14 16:03:40 +0530

    templating(views) using ejs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const winston=require('winston');
 const express=require('express');
 const app=express();
 
+app.set('view engine', 'ejs');
+
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
